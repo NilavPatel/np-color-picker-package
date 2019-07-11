bc3d55cf71e25abf22ecc2dda79b78abd4d7f355
Nilav Patel <nilavpatel1992@gmail.com>
2019-07-11 19:46:53 +0530

    add canvas to pick color

--- projects/np-color-picker/src/lib/np-color-picker.component.ts
+++ projects/np-color-picker/src/lib/np-color-picker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output, Input, SimpleChanges } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'np-color-picker',
@@ -7,42 +8,28 @@ import { Component, OnInit, EventEmitter, Output, Input, SimpleChanges } from '@
 })
 export class NpColorPickerComponent implements OnInit {
 
-  _colorList: string[];
   _value: string;
   _isOpen: boolean = false;
 
   @Input() value: string
   @Output() valueChange = new EventEmitter();
   @Input() defaultOpen: boolean;
-  @Input() colorList: string[];
   @Output() onChange: EventEmitter<any> = new EventEmitter();
   @Input() disabled: boolean;
-  @Input() height: string;
-  @Input() width: string;
 
-  constructor() {
+  _primaryColor: string = "#0000ff";
+
+  public context: CanvasRenderingContext2D;
+
+  constructor(private elRef: ElementRef) {
   }
 
   ngOnInit() {
-    if (this.colorList == undefined) {
-      this._colorList = [
-        '#FFA07A', '#FA8072', '#E9967A', '#F08080', '#CD5C5C', '#DC143C', '#B22222', '#FF0000', '#8B0000',
-        '#FF7F50', '#FF6347', '#FF4500', '#FFD700', '#FFA500', '#FF8C00', '#FFFFE0', '#FFFACD', '#FAFAD2',
-        '#FFEFD5', '#FFE4B5', '#FFDAB9', '#EEE8AA', '#F0E68C', '#BDB76B', '#FFFF00', '#7CFC00', '#7FFF00',
-        '#32CD32', '#00FF00', '#228B22', '#008000', '#006400', '#ADFF2F', '#9ACD32', '#00FF7F', '#00FA9A',
-        '#90EE90', '#98FB98', '#8FBC8F', '#3CB371', '#2E8B57', '#808000', '#556B2F', '#6B8E23', '#E0FFFF',
-        '#00FFFF', '#00FFFF', '#7FFFD4', '#66CDAA', '#AFEEEE', '#40E0D0', '#48D1CC', '#00CED1', '#20B2AA',
-        '#5F9EA0', '#008B8B', '#008080', '#B0E0E6', '#ADD8E6', '#87CEFA', '#87CEEB', '#00BFFF', '#B0C4DE',
-        '#1E90FF', '#6495ED', '#4682B4', '#4169E1', '#0000FF', '#0000CD', '#00008B', '#000080', '#191970',
-        '#7B68EE', '#6A5ACD', '#483D8B', '#E6E6FA', '#D8BFD8', '#DDA0DD', '#EE82EE', '#DA70D6', '#FF00FF',
-        '#FF00FF', '#BA55D3', '#9370DB', '#8A2BE2', '#9400D3', '#9932CC', '#8B008B', '#800080', '#4B0082',
-        '#FFC0CB', '#FFB6C1', '#FF69B4', '#FF1493', '#DB7093', '#C71585', '#FFFFFF', '#FFFAFA', '#F0FFF0',
-        '#F5FFFA', '#F0FFFF', '#F0F8FF', '#F8F8FF', '#F5F5F5', '#FFF5EE', '#F5F5DC', '#FDF5E6', '#FFFAF0',
-        '#FFFFF0', '#FAEBD7', '#FAF0E6', '#FFF0F5', '#FFE4E1', '#DCDCDC', '#D3D3D3', '#C0C0C0', '#A9A9A9',
-        '#808080', '#696969', '#778899', '#708090', '#2F4F4F', '#000000', '#FFF8DC', '#FFEBCD', '#FFE4C4',
-        '#FFDEAD', '#F5DEB3', '#DEB887', '#D2B48C', '#BC8F8F', '#F4A460', '#DAA520', '#CD853F', '#D2691E',
-        '#8B4513', '#A0522D', '#A52A2A', '#800000',
-      ];
+  }
+
+  ngAfterViewInit() {
+    if (this.defaultOpen) {
+      this._updateCanvas();
     }
   }
 
@@ -53,23 +40,15 @@ export class NpColorPickerComponent implements OnInit {
         this.onChange.emit(this._value);
       }
     }
-    if (changes.colorList != undefined) {
-      this._colorList = changes.colorList.currentValue;
-    }
-  }
-
-  _onClickColor(color: string) {
-    this._value = color;
-    this.value = color;
-    this.valueChange.emit(color);
-    this._isOpen = false;
-    if (this.onChange != undefined) {
-      this.onChange.emit(color);
-    }
   }
 
   _toggleColorPicker() {
     this._isOpen = !this._isOpen
+    if (this._isOpen) {
+      setTimeout(() => {
+        this._updateCanvas();
+      }, 100);
+    }
   }
 
   _close() {
@@ -83,4 +62,74 @@ export class NpColorPickerComponent implements OnInit {
       this.onChange.emit(this._value);
     }
   }
+
+  _updateCanvas() {
+    var block = <HTMLCanvasElement>this.elRef.nativeElement.querySelector('.np-canvas-block');
+    var strip = <HTMLCanvasElement>this.elRef.nativeElement.querySelector('.np-canvas-strip');
+    var ctx1 = block.getContext('2d');
+    var ctx2 = strip.getContext('2d');
+
+    ctx1.fillStyle = this._primaryColor.length > 0 ? this._primaryColor : this.value;
+    ctx1.fillRect(0, 0, 200, 200);
+
+    var grdWhite = ctx2.createLinearGradient(0, 0, 200, 0);
+    grdWhite.addColorStop(0, 'rgba(255,255,255,1)');
+    grdWhite.addColorStop(1, 'rgba(255,255,255,0)');
+    ctx1.fillStyle = grdWhite;
+    ctx1.fillRect(0, 0, 200, 200);
+
+    var grdBlack = ctx2.createLinearGradient(0, 0, 0, 200);
+    grdBlack.addColorStop(0, 'rgba(0,0,0,0)');
+    grdBlack.addColorStop(1, 'rgba(0,0,0,1)');
+    ctx1.fillStyle = grdBlack;
+    ctx1.fillRect(0, 0, 200, 200);
+
+    ctx2.rect(0, 0, 30, 150);
+    var grd1 = ctx2.createLinearGradient(0, 0, 0, 150);
+    grd1.addColorStop(0, 'rgba(255, 0, 0, 1)');
+    grd1.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
+    grd1.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
+    grd1.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
+    grd1.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
+    grd1.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
+    grd1.addColorStop(1, 'rgba(255, 0, 0, 1)');
+    ctx2.fillStyle = grd1;
+    ctx2.fill();
+  }
+
+  clickStrip(e: any) {
+    var strip = <HTMLCanvasElement>this.elRef.nativeElement.querySelector('.np-canvas-strip');
+    var ctx2 = strip.getContext('2d');
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var imageData = ctx2.getImageData(x, y, 1, 1).data;
+    this._primaryColor = this.fullColorHex(imageData[0], imageData[1], imageData[2]);
+    this._updateCanvas();
+  }
+
+  changeColor(e: any) {
+    var block = <HTMLCanvasElement>this.elRef.nativeElement.querySelector('.np-canvas-block');
+    var ctx1 = block.getContext('2d');
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var imageData = ctx1.getImageData(x, y, 1, 1).data;
+    this._value = this.fullColorHex(imageData[0], imageData[1], imageData[2]);
+    this.value = this._value;
+    this.valueChange.emit(this._value);
+  }
+
+  fullColorHex(r: number, g: number, b: number) {
+    var red = this.rgbToHex(r);
+    var green = this.rgbToHex(g);
+    var blue = this.rgbToHex(b);
+    return "#" + red + green + blue;
+  };
+
+  rgbToHex(rgb: number) {
+    var hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+      hex = "0" + hex;
+    }
+    return hex;
+  };
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,7 @@ export class AppComponent {
   currentColor1: string = "#00FF00";
   currentColor2: string;
   currentColor3: string;
-  colorList2: string[] = ['#FFFFFF', '#000000', '#ff0000', '#ff4000', '#ff8000'];
-
+  
   onColorSelect(value: string) {
     alert("selected color is " + value);
   }
